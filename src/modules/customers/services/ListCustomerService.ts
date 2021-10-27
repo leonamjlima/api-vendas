0d9c9c131a6b8f6755546884c939d15f69f5cfd2
Leonam Lima <leonamjlima@outlook.com>
2021-10-27 14:19:09 -0300

    .
    .

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -2,35 +2,12 @@ import { getCustomRepository } from 'typeorm';
 import Customer from '../typeorm/entities/Customer';
 import { CustomersRepository } from '../typeorm/repositories/CustomersRepository';
 
-interface IPaginateCustomer {
-  from: number;
-  to: number;
-  per_page: number;
-  total: number | number;
-  current_page: number;
-  prev_page?: number | null;
-  next_page?: number | null;
-  last_page: number | null;
-  data: Customer[];
-}
-
 class ListCustomerService {
-  public async execute(): Promise<IPaginateCustomer | undefined> {
-    try {
-      const customersRepository = getCustomRepository(CustomersRepository);
+  public async execute(): Promise<Customer[]> {
+    const customersRepository = getCustomRepository(CustomersRepository);
+    const customers = await customersRepository.find();
 
-      const customers = await customersRepository
-        .createQueryBuilder()
-        .paginate();
-
-      // const customers = await customersRepository
-      //   .createQueryBuilder('customers')
-      //   .paginate(15);
-
-      return customers as IPaginateCustomer;
-    } catch (error) {
-      console.log(error);
-    }
+    return customers;
   }
 }
 
